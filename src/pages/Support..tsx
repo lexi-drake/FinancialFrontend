21a1587f06bb4864dff72bc3874bed014e92c0c9
alexa <alexa11drake@gmail.com>
2021-02-14 10:38:44 -0600

    removed unused import

---
+++ src/pages/Support..tsx
@@ -0,0 +1,17 @@
+import Container from "../components/custom/Container"
+import Header from "../components/custom/Header"
+
+interface SupportProps {
+
+}
+
+const Support = (props: SupportProps) => {
+
+    return (
+        <Container>
+            <Header>
+                <h1>Support</h1>
+            </Header>
+        </Container>
+    );
+}
